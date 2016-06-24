a1e413c87aa01420014fe514ea5a3d714318a2cf
Raffael Kessler <raffael.kessler@prowect.com>
2016-06-24 07:38:50 +0200

    Formatiert

--- src/Config.php
+++ src/Config.php
@@ -1,19 +1,31 @@
 <?php
+
 namespace Drips\Config;
+
 class Config
 {
     protected static $collection = array();
 
-    public static function get($key, $default=null) {
+    private function __construct(){}
+
+    private function __clone(){}
+
+    public static function get($key, $default = null)
+    {
         if (static::has($key)) {
             return static::$collection[$key];
         }
+
         return $default;
     }
-    public static function set($key, $value) {
+
+    public static function set($key, $value)
+    {
         static::$collection[$key] = $value;
     }
-    public static function has($key) {
+
+    public static function has($key)
+    {
         return array_key_exists($key, static::$collection);
     }
 }
--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -1,20 +1,22 @@
 <?php
 
 namespace tests;
+
 use PHPUnit_Framework_TestCase;
 use Drips\Config\Config;
+
 class ConfigTest extends PHPUnit_Framework_TestCase
 {
-   public function testDC(){
-       $key = "key";
-       $value = "value";
-       $key2 = "key2";
-       $value2 = "value2";
+   public function testDC()
+   {
+       $key = 'key';
+       $value = 'value';
+       $key2 = 'key2';
+       $value2 = 'value2';
        $this->assertFalse(Config::has($key));
-       Config::set($key,$value);
+       Config::set($key, $value);
        $this->assertTrue(Config::has($key));
        $this->assertEquals(Config::get($key), $value);
        $this->assertEquals(Config::get($key2, $value2), $value2);
-
    }
 }
